3617439d9fdaca7b52accb6f70f987ad1a4cee02
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-05-17 18:12:07 -0500

    Implementacion de Delete en TestAVL

--- bstreelinklistinterfgeneric/TestAVL.java
+++ bstreelinklistinterfgeneric/TestAVL.java
@@ -8,7 +8,7 @@ public class TestAVL {
             arbol.insert(30);
             arbol.insert(20);
             System.out.println(arbol.drawBST());
-            
+
             arbol.insert(10); // Rotación simple a la derecha
             System.out.println(arbol.drawBST());
             
@@ -34,6 +34,32 @@ public class TestAVL {
 
             arbol.insert(65); // Rotación doble a la izquierda
             System.out.println(arbol.drawBST());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\nEliminaciones con rotaciones\n");
+
+        AVLTree<Integer> arbol2 = new AVLTree<>();
+
+        try {
+            arbol2.insert(50);
+            arbol2.insert(30);
+            arbol2.insert(70);
+            arbol2.insert(20);
+            arbol2.insert(40);
+            arbol2.insert(60);
+            arbol2.insert(80);
+            arbol2.insert(10);
+            arbol2.insert(25);
+
+            System.out.println("Antes de eliminar 30:");
+            System.out.println(arbol2.drawBST());
+
+            arbol2.delete(30);
+            System.out.println("Después de eliminar 30:");
+            System.out.println(arbol2.drawBST());
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
